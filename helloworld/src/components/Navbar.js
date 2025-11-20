3cecd803ee85a42b600f34a02f78e9fd54235ea8
Salm-a-n <salmanntechs@gmail.com>
2025-11-20 15:51:51 +0530

    updated

--- helloworld/src/components/Navbar.js
+++ helloworld/src/components/Navbar.js
@@ -1,7 +1,24 @@
-import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { removeUser } from "../store/authSlice";
 
 function Navbar() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    var user = useSelector(store=>store.auth.user);
+    function logout(){
+        if(user){
+            axios.post('https://demo-blog.mashupstack.com/api/logout',{},{
+               headers:{'Authorization':"Bearer "+ user.token}
+            });
+            dispatch(removeUser());
+            navigate('/login');
+        }
+    }
+
     return <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        
         <div className="navbar-brand">
             <h4>My app</h4>
         </div>
@@ -36,6 +53,25 @@ function Navbar() {
                 <NavLink to={"/blog/posts"} className={ 'nav-link '+(status => status.isActive ? 'active' : '')}>
                     blog
                 </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink 
+                    to={"/register"} 
+                    className={
+                        'nav-link '+
+                        (status => status.isActive ? 'active' : '')
+                    } 
+                    >
+                        Register
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink to={"/Login"} className={ 'nav-link '+(status => status.isActive ? 'active' : '')}>
+                    Login
+                </NavLink>
+                </li>
+                 <li className="nav-item">
+                <span className="nav-link" onClick={logout}>Logout</span>
                 </li>
             </ul>
         </div>
--- helloworld/src/router.js
+++ helloworld/src/router.js
@@ -5,6 +5,8 @@ import ListPosts from "./components/blog/ListPost";
 import CreatePost from "./components/blog/CreatePost";
 import ViewPost from "./components/blog/Viewpost";
 import EditPost from "./components/blog/EditPost";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const router = createBrowserRouter([
     { path: '', element: <App/> },
@@ -12,7 +14,9 @@ const router = createBrowserRouter([
     { path: 'blog/posts/',element: <ListPosts/>},
     { path : 'blog/posts/create' , element : <CreatePost/> },
     { path: 'blog/posts/:postId', element: <ViewPost/>},
-    { path : '/blog/posts/:postId/edit', element: <EditPost/>}
+    { path : '/blog/posts/:postId/edit', element: <EditPost/>},
+    { path: 'register', element:<Register/>},
+    {path: 'Login',element:<Login/>},
 ]);
 
 export default router;
